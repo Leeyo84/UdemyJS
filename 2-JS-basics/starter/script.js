e16367865fa18a360b3c0114bdac3169937cde59
Leonela <Leonela_Gutierrez@hotmail.com>
2020-03-25 19:30:36 -0600

    completed basic of variable mutation and coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,8 @@
+/******************
+* Variables and Data types
+
+
+
 console.log("Hello World!!!");
 console.log("do it");
 
@@ -18,7 +23,64 @@ console.log(job);
 
 job = 'teacher';
 console.log(job);
+ * */
 
 // var 3years = 3;
 //you can not start a variable with a number or it may not contain symbols
 //or reserved JS key words
+
+/**************
+ * Variable mutation and type coercion
+ * */
+
+var firstName = 'John';
+var age = 28;
+
+// Type Coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
