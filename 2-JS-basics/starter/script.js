458f9f4ce1fea360a1d5f5ab7a77fd4777a8d3df
Leonela <Leonela_Gutierrez@hotmail.com>
2020-03-25 19:13:55 -0600

    finished fundamentals of variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -9,3 +9,16 @@ console.log(firstName);
 
 var lastName = 'Smith';
 var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+// var 3years = 3;
+//you can not start a variable with a number or it may not contain symbols
+//or reserved JS key words
