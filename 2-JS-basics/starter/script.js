f6ab2cb3105dc69dcd3dc645e894cd53d13f9b09
Leonela <Leonela_Gutierrez@hotmail.com>
2020-03-27 18:03:40 -0600

    working on if/else

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -127,30 +127,63 @@ console.log(job);
  * Coding Challenge 1
  * */
 
-markHeight = 1.98;
-markMass = 95;
-
-johnHeight = 1.69;
-johnMass = 78;
-
-mark = markHeight, markMass;
-
-john = johnHeight, johnMass;
-
-
-
-
-johnBMI = johnMass / (johnHeight * johnHeight);
-markBMI = markMass / (markHeight * markHeight);
-
-markHigherBMI = (markBMI > johnBMI);
-console.log(markHigherBMI);
-
-console.log("Is Mark's BMI higher than John's?" + ' ' + markHigherBMI);
-
-
-
 // solution
+// markHeight = 1.98;
+// markMass = 95;
+//
+// johnHeight = 1.69;
+// johnMass = 78;
+//
+// mark = markHeight, markMass;
+//
+// john = johnHeight, johnMass;
+//
+//
+//
+//
+// johnBMI = johnMass / (johnHeight * johnHeight);
+// markBMI = markMass / (markHeight * markHeight);
+//
+// markHigherBMI = (markBMI > johnBMI);
+// console.log(markHigherBMI);
+//
+// console.log("Is Mark's BMI higher than John's?" + ' ' + markHigherBMI);
+
+// var massMark = 78; // kg
+// var heightMark = 1.69; // meters
+//
+// var massJohn = 92;
+// var heightJohn = 1.95;
+//
+// var BMIMark = massMark/ (heightMark * heightMark);
+// var BMIJohn = massJohn/ (heightJohn * heightJohn);
+//
+// var markHigherBMI = BMIMark > BMIJohn;
+// console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+
+
+/***********************************
+ * If / else statements
+ */
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+
+
 
 var massMark = 78; // kg
 var heightMark = 1.69; // meters
@@ -161,8 +194,16 @@ var heightJohn = 1.95;
 var BMIMark = massMark/ (heightMark * heightMark);
 var BMIJohn = massJohn/ (heightJohn * heightJohn);
 
-var markHigherBMI = BMIMark > BMIJohn;
-console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+// var markHigherBMI = BMIMark > BMIJohn;
+// console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+
+if (BMIMark > BMIJohn) {
+    console.log('Mark\'s BMI is higher than John\'s.');
+} else {
+    console.log('John\'s BMI is higher than Mark\'s.');
+}
+
+
 
 
 
